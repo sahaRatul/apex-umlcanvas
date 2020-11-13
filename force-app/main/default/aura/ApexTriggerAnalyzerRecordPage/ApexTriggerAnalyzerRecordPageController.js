367f69d13d803a52b93b290018318a7556365dcf
Ratul Saha <saharatul@pm.me>
2020-11-13 19:59:07 +0530

    Record page UI v1 added

--- force-app/main/default/aura/ApexTriggerAnalyzerRecordPage/ApexTriggerAnalyzerRecordPageController.js
+++ force-app/main/default/aura/ApexTriggerAnalyzerRecordPage/ApexTriggerAnalyzerRecordPageController.js
@@ -1,5 +1,12 @@
 ({
-    myAction : function(component, event, helper) {
-
+    init : function(component, event, helper) {
+        let dummyData = helper.getDummyData();
+        component.set('v.triggerAnalyzerReport', dummyData);
+        component.set('v.selectedObject', dummyData[0]);
+    },
+    objectNameClickHandler: function(component, event, helper) {
+        let selectedVal = event.currentTarget.dataset.value;
+        let selectedObject = component.get('v.triggerAnalyzerReport').filter(x => x.objectApiName === selectedVal);
+        component.set('v.selectedObject', selectedObject[0] ? selectedObject[0] : component.get('v.triggerAnalyzerReport')[0]);
     }
 })
--- force-app/main/default/aura/ApexTriggerAnalyzerRecordPage/ApexTriggerAnalyzerRecordPageHelper.js
+++ force-app/main/default/aura/ApexTriggerAnalyzerRecordPage/ApexTriggerAnalyzerRecordPageHelper.js
@@ -1,5 +1,124 @@
 ({
-    helperMethod : function() {
-
+    getDummyData: function () {
+        return [{
+            objectName: 'Account',
+            objectApiName: 'Account',
+            triggerDetails: {
+                activeTriggers: ['test1', 'test2', 'test3'],
+                inactiveTriggers: ['test4', 'test24', 'test233'],
+                invalidTriggers: ['test231', 'test223', 'test323'],
+                deletedTriggers: ['test123', 'test2232', 'test3323'],
+                managedTriggers: ['test123', 'test2233', 'test32323']
+            },
+            duplicatedEvents: [{
+                eventName: 'Before Insert',
+                triggerName: ['xyz', 'sdfsf']
+            }, {
+                eventName: 'Before Update',
+                triggerName: ['abc', 'uvw']
+            }, {
+                eventName: 'Before Delete',
+                triggerName: ['cde, fgh']
+            }, {
+                eventName: 'After Insert',
+                triggerName: ['test1', 'test2343']
+            }],
+            hardcodedIds: [{
+                id: '1213232323',
+                triggerName: 'abcxyz',
+                lineNumber: 2
+            },
+            {
+                id: '1213232323',
+                triggerName: 'abcxyz',
+                lineNumber: 2
+            },
+            {
+                id: '1213232323',
+                triggerName: 'abcxyz',
+                lineNumber: 2
+            }],
+            loopDMLs: [{
+                statement: 'select xyz from account',
+                triggerName: 'dsfsdfsf',
+                lineNumber: 12,
+            }, {
+                statement: 'select xyz from account',
+                triggerName: 'dsfsdfsf',
+                lineNumber: 12,
+            }],
+            queriesWithoutClause: [{
+                statement: 'select xyz from account',
+                triggerName: 'dsfsdfsf',
+                lineNumber: 12,
+            }, {
+                statement: 'select xyz from account',
+                triggerName: 'dsfsdfsf',
+                lineNumber: 12,
+            }, {
+                statement: 'select xyz from account',
+                triggerName: 'dsfsdfsf',
+                lineNumber: 12,
+            }]
+        },
+        {
+            objectName: 'Opportunity',
+            objectApiName: 'Opportunity',
+            triggerDetails: {
+                activeTriggers: ['test1', 'test3'],
+                inactiveTriggers: ['test233'],
+                invalidTriggers: ['test323'],
+                deletedTriggers: ['test3323'],
+                managedTriggers: ['test123', 'test2233', 'test32323']
+            },
+            duplicatedEvents: [{
+                eventName: 'Before Insert',
+                triggerName: ['xyz', 'sdfsf']
+            }, {
+                eventName: 'Before Delete',
+                triggerName: ['cde, fgh']
+            }, {
+                eventName: 'After Insert',
+                triggerName: ['test1', 'test2343']
+            }],
+            hardcodedIds: [{
+                id: '1213232323',
+                triggerName: 'abcxyz',
+                lineNumber: 2
+            }]
+        },
+        {
+            objectName: 'Lead',
+            objectApiName: 'Lead',
+            triggerDetails: {
+                activeTriggers: ['Lead1', 'Lead2', 'Lead3'],
+                inactiveTriggers: ['test4', 'test24'],
+                invalidTriggers: ['test231'],
+                deletedTriggers: ['test123'],
+                managedTriggers: ['test123', 'test2233', 'test32323']
+            }
+        },
+        {
+            objectName: 'User',
+            objectApiName: 'User',
+            triggerDetails: {
+                activeTriggers: ['test1', 'test3'],
+                inactiveTriggers: ['test4', 'test24', 'test233'],
+                invalidTriggers: ['test231', 'test323'],
+                deletedTriggers: ['test123', 'test2232', 'test3323'],
+                managedTriggers: ['test123', 'test32323']
+            }
+        },
+        {
+            objectName: 'Case',
+            objectApiName: 'Case',
+            triggerDetails: {
+                activeTriggers: ['test1', 'test2', 'test3'],
+                inactiveTriggers: ['test4', 'test24', 'test233'],
+                invalidTriggers: ['test231', 'test223', 'test323'],
+                deletedTriggers: ['test123', 'test2232', 'test3323'],
+                managedTriggers: ['test123', 'test2233', 'test32323']
+            }
+        }];
     }
 })
