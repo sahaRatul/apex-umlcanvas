f9488636480921a9afcf91cdf513df2379a1b9cb
Ratul Saha <saharatul@pm.me>
2020-11-19 09:13:30 +0530

    changes added

--- force-app/main/default/aura/ApexTriggerAnalyzerRecordPage/ApexTriggerAnalyzerRecordPageController.js
+++ force-app/main/default/aura/ApexTriggerAnalyzerRecordPage/ApexTriggerAnalyzerRecordPageController.js
@@ -1,8 +1,19 @@
 ({
     init : function(component, event, helper) {
         let action = component.get("c.getStoredReport");
+        var recIdfromVf = component.get("v.recordIdFromVf");
+        var recordIdComp;
+        if(!recIdfromVf){
+            console.log("yyyyy");
+            recordIdComp = component.get("v.recordId");
+        }else{
+            console.log("xxxxxx");
+            recordIdComp = recIdfromVf;
+        }
+        
+        console.log(recIdfromVf  + ")))" + recordIdComp);
         action.setParams({
-            reportId: component.get('v.recordId')
+            reportId: recordIdComp
         });
         action.setCallback(this, function (result) {
             let state = result.getState();
@@ -18,5 +29,18 @@
         let selectedVal = event.currentTarget.dataset.value;
         let selectedObject = component.get('v.triggerAnalyzerReport').filter(x => x.objectApiName === selectedVal);
         component.set('v.selectedObject', selectedObject[0] ? selectedObject[0] : component.get('v.triggerAnalyzerReport')[0]);
+    },
+    handleSectionToggle: function (cmp, event) {
+        var openSections = event.getParam('openSections');
+
+        if (openSections.length === 0) {
+            cmp.set('v.activeSectionsMessage', "All sections are closed");
+        } else {
+            cmp.set('v.activeSectionsMessage', "Open sections: " + openSections.join(', '));
+        }
+    },
+    onmousehover : function(component, event, helper) {
+        console.log('=onmousehover===');
+        component.set('v.isTooltip', true);
     }
 })
